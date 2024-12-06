04c49d8f15667fe221ce0a1f81c787b51442300d
lukemcdowell <luke.db.mcdowell@gmail.com>
2024-12-06 20:56:44 +0000

    2024 day 6 part 2

--- 2024/day6/main.py
+++ 2024/day6/main.py
@@ -1,3 +1,6 @@
+import time
+
+
 class Main:
     def __init__(self, input_file):
         self.data = self.get_data(input_file)
@@ -9,6 +12,7 @@ class Main:
         # directions and order
         self.dirs = {"^": (-1, 0), ">": (0, 1), "v": (1, 0), "<": (0, -1)}
         self.dir_order = ["^", ">", "v", "<"]
+        # could have just used one list and stored dir as an int
 
     def get_data(self, filename):
         with open(filename, "r") as file:
@@ -51,9 +55,9 @@ class Main:
         return len(visited_positions)
 
     def part_two(self):
-        possible_obstacles = set()
-        turns = []
-        guard_pos = None
+        total = 0
+        guard_pos, guard_dir = None, None
+        start_pos, start_dir = None, None
 
         # find start position and direction
         for x in range(self.rows):
@@ -61,12 +65,18 @@ class Main:
                 break
             for y in range(self.cols):
                 if self.grid[x][y] in self.dirs:
-                    guard_dir = self.grid[x][y]
+                    start_pos = (x, y)
+                    start_dir = self.grid[x][y]
                     guard_pos = (x, y)
+                    guard_dir = self.grid[x][y]
                     break
 
+        # get a list of all visited positions
+        visited_positions = set()
         while True:
             x, y = guard_pos
+            if guard_pos not in visited_positions:
+                visited_positions.add(guard_pos)
 
             # calculate next position
             dx, dy = self.dirs[guard_dir]
@@ -79,31 +89,51 @@ class Main:
             # turn right if obstacle,
             if self.grid[nx][ny] == "#":
                 guard_dir = self.dir_order[(self.dir_order.index(guard_dir) + 1) % 4]
-                turns.append((x, y))
             # move forward
             else:
-
-                # check for possible infinite loops by placing obstacles
-                # currently only works for rectangles
-                if len(turns) >= 4:
-                    # check if placing an obsctacle infront would make a loop
-                    # to make a loop, current position would have to intercept the line
-                    # made between two turns ago and three turns ago
-                    x1, y1 = (x, y)  # current point
-                    x2, y2 = turns[-3]  # two turns ago
-                    x3, y3 = turns[-4]  # three turns ago
-
-                    # check that a, b and c are on the same line
-                    # https://math.stackexchange.com/questions/405966/if-i-have-three-points-is-there-an-easy-way-to-tell-if-they-are-collinear
-                    if (y1 - y2) * (x1 - x3) == (y1 - y3) * (x1 - x2):
-                        if (nx, ny) not in possible_obstacles:
-                            possible_obstacles.add((nx, ny))
-
                 guard_pos = (nx, ny)
 
-        return len(possible_obstacles)
+        def check_loop(pos, dir):
+            num_moves = 0
+
+            while True:
+                max_moves = self.rows * self.cols * 4 + 1
+                if num_moves > max_moves:
+                    return True
+
+                x, y = pos
+
+                # calculate next position
+                dx, dy = self.dirs[dir]
+                nx, ny = x + dx, y + dy
+
+                # check if out of bounds
+                if not (0 <= nx < self.rows and 0 <= ny < self.cols):
+                    return False
+
+                # turn right if obstacle,
+                if self.grid[nx][ny] == "#":
+                    dir = self.dir_order[(self.dir_order.index(dir) + 1) % 4]
+                # move forward
+                else:
+                    pos = (nx, ny)
+
+                num_moves += 1
+
+        for x in range(self.rows):
+            for y in range(self.cols):
+                # check all visited positions and see if placing a # there causes an infinite loop
+                if (x, y) in visited_positions and self.grid[x][y] == ".":
+                    print("check: ", (x, y))
+                    self.grid[x][y] = "#"
+                    if check_loop(start_pos, start_dir):
+                        total += 1
+                    self.grid[x][y] = "."
+
+        return total
 
 
 if __name__ == "__main__":
-    day6 = Main("sample.txt")
+    day6 = Main("input.txt")
     print("Part One Total:", day6.part_one())
+    print("Part Two Total:", day6.part_two())
